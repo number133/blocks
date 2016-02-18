51bd35b32a6af84afcb0de6d3d6f8a52ad272e66
Abylay.Sabirgaliyev <Abylay.Sabirgaliyev@jazzsoft.kz>
2016-02-18 17:57:37 +0600

    Other logic

--- js/custom.js
+++ js/custom.js
@@ -20,23 +20,21 @@ var Custom = Custom || {};
 /* TextBlock */
 Custom.TextBlock = function (game, indexX, indexY, width, height, text, color) {
     'use strict';
-    var style = { font: "20px Courier", fill: "#fff", backgroundColor: color};
+    var style = { font: "14px Arial", fill: "#000", backgroundColor: color, align: "center"};
     Phaser.Text.call(this, game, width * indexX, height * indexY, text, style);
     this.width = width;
     this.height = height;
     this.indexX = indexX;
-    this.indexY = indexY;    
+    this.indexY = indexY;
 };
 Custom.TextBlock.prototype = Object.create(Phaser.Text.prototype);
 Custom.TextBlock.constructor = Custom.TextBlock;
-Custom.TextBlock.prototype.init = function () {
-    'use strict';
-};
 
 /* Board */
 Custom.Board = function (game, blockWidth, blockHeight, maxX, maxY) {
     'use strict';    
     this.blocks = [];
+    this.selectedBlock = null;
     for (var x = 0; x < maxX; x++)
     {
         var a = [];
@@ -65,8 +63,25 @@ Custom.Board.prototype.isFree = function (x, y) {
     return this.blocks[x][y] === null;
 };
 
+Custom.Board.prototype.selectBlock = function (block) {
+    if(this.selectedBlock != null) {
+        this.deselectBlock(this.selectedBlock);
+    }
+    this.selectedBlock = block;
+    block.tint = "0x999999";
+};
+
+Custom.Board.prototype.deselectBlock = function (block) {
+    this.selectedBlock = null;
+    block.tint = "0xFFFFFF";
+};
+
+Custom.Board.prototype.isSelected = function (block) {
+    return this.selectedBlock === block;
+};
+
 // create
-Custom.Board.prototype.getBlock = function (text, color, indexX, indexY) {	
+Custom.Board.prototype.getBlock = function (text, color, data, indexX, indexY) {	
     var textblock = new Custom.TextBlock(
     	this.game, 
     	indexX, 
@@ -77,30 +92,39 @@ Custom.Board.prototype.getBlock = function (text, color, indexX, indexY) {
     	color
     );
     this.blocks[indexX][indexY] = textblock;
+    textblock.data = data;
     textblock.inputEnabled = true;
-    textblock.events.onInputDown.add(function(block){
-        this.explodeFrom(block);
-        if(this.isGameOver()){
-            var style = { font: "40px Courier", fill: "#fff", tabs: 80 };
-            this.game.add.text(0, 0, "Game over, loser!", style);
-            var music = game.add.audio('loser');
-            music.play();
+    textblock.events.onInputDown.add(function(block){        
+        if(this.isSelected(block)){
+            this.deselectBlock(block);
+        } else {
+            if(this.getNeibs(block).contains(this.selectedBlock)){
+                this.swap(block, this.selectedBlock);
+                this.deselectBlock(this.selectedBlock);
+                this.explode();
+                if(!this.hasSameBlocks()){
+                    var style = { font: "40px Arial", fill: "#fff", align: "center" };
+                    this.game.add.text(0, 0, "Game over!", style);
+                }
+            } else {
+                this.selectBlock(block);
+            }
         }
     }, this);
     return textblock;
 }
 
-Custom.Board.prototype.addBlock = function (text, color, indexX, indexY) {
-	var block = this.getBlock(text, color, indexX, indexY);
+Custom.Board.prototype.addBlock = function (text, color, data, indexX, indexY) {
+	var block = this.getBlock(text, color, data, indexX, indexY);
 	this.game.add.existing(block);
 	return block;
 }
 
-Custom.Board.prototype.pushBlock = function (text, color) {
+Custom.Board.prototype.pushBlock = function (text, color, data) {
     for (var y = 0; y < this.maxY; y++) {
         for (var x = 0; x < this.maxX; x++) {
             if(this.blocks[x][y] === null){
-                return this.addBlock(text, color, x, y);            
+                return this.addBlock(text, color, data, x, y);            
             }
         }
     }
@@ -139,6 +163,26 @@ Custom.Board.prototype.moveDown = function (block) {
         block.indexY += 1;
         block.y += block.height;
     }
+}
+
+Custom.Board.prototype.swap = function (b1, b2) {
+    var bufX, bufY, bufIndexX, bufIndexY;
+    bufX = b1.x;
+    bufY = b1.y;
+    bufIndexX = b1.indexX;
+    bufIndexY = b1.indexY;
+    
+    b1.x = b2.x;
+    b1.y = b2.y;
+    b1.indexX = b2.indexX;
+    b1.indexY = b2.indexY;
+    this.blocks[b2.indexX][b2.indexY] = b1;
+
+    b2.x = bufX;
+    b2.y = bufY;
+    b2.indexX = bufIndexX;
+    b2.indexY = bufIndexY;
+    this.blocks[bufIndexX][bufIndexY] = b2;
 }
 
 // squeeze
@@ -233,11 +277,12 @@ Custom.Board.prototype.explode = function () {
     toDelete = toDelete.unique();
     this.removeAll(toDelete);
     if(toDelete.length > 0){
-        this.pullDown();
+        this.squeeze();
+        this.explode();
     }
 }
 
-Custom.Board.prototype.isGameOver = function () {
+Custom.Board.prototype.hasReadyGroups = function () {
     var toDelete = [];
     for(var y = 0; y < this.maxY; y++) {
         for (var x = 0; x < this.maxX; x++) {
@@ -248,6 +293,22 @@ Custom.Board.prototype.isGameOver = function () {
     }
     toDelete = toDelete.unique();
     return toDelete.length === 0;
+}
+
+Custom.Board.prototype.hasSameBlocks = function () {
+    var unique = [];
+    for(var y = 0; y < this.maxY; y++) {
+        for (var x = 0; x < this.maxX; x++) {
+            if(this.blocks[x][y] != null){
+                if(unique.contains(this.blocks[x][y].data.id)){
+                    return true;
+                } else {
+                    unique.push(this.blocks[x][y].data.id);
+                }
+            }
+        }
+    }
+    return false;
 }
 
 Custom.Board.prototype.explodeFrom = function (block) {
@@ -264,7 +325,7 @@ Custom.Board.prototype.explodeFrom = function (block) {
 Custom.Board.prototype.collectBlocks = function (block, toDelete) {
     var neibs = this.getNeibs(block);
     for(var i=0; i < neibs.length; i++){
-        if(block.style.backgroundColor === neibs[i].style.backgroundColor){
+        if(block.data.id === neibs[i].data.id){
             toDelete.push(block);
             toDelete.push(neibs[i]);
             block.collected = true;
--- js/second.js
+++ js/second.js
@@ -2,24 +2,86 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, 
 	update: update, render: render });
 // Colors
-var red = "#f00", black = "#fff", blue = "#00f", green = "#0f0", white = "#000";
-var colors = [red, blue, green, white];
+var red = "#f00", blue = "#00f", green = "#0f0", white = "#fff", yellow = "#ff0";
+var colors = [red, blue, green, white, yellow, "#ffa500", "#f6546a"];
 // Vars
 var board, block;
 function preload() {
     game.load.audio('loser', 'sound/loser.mp3');
 }
 
+var words = [
+	{
+		en: "boy",
+		kz: "bala"
+	},
+	{
+		en: "girl",
+		kz: "kyz"
+	},
+	{
+		en: "moon",
+		kz: "ai"
+	},
+	{
+		en: "sun",
+		kz: "kun"
+	},
+	{
+		en: "blood",
+		kz: "kan"
+	},
+	{
+		en: "soul",
+		kz: "zhan"
+	},
+	{
+		en: "men",
+		kz: "adam"
+	},
+	{
+		en: "hand",
+		kz: "kol"
+	},
+	{
+		en: "head",
+		kz: "bas"
+	},
+	{
+		en: "dog",
+		kz: "it"
+	},
+	{
+		en: "cat",
+		kz: "mysyk"
+	}
+];
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
 // crate function
 function create() {
 	game.stage.backgroundColor = '#505050';
 	
-    board = new Custom.Board(game, 100, 40, 5, 10);	
+    board = new Custom.Board(game, 120, 40, 5, 10);	
     // block = factory.pushBlock("Test");
-    for(var k = 0; k < 40; k ++){
-    	board.pushBlock(" ", colors[board.getRandom(4)-1]);
-    }
-    board.pullDown();
+    // for(var k = 0; k < 40; k ++){
+    // 	board.pushBlock("Test", colors[board.getRandom(4)-1]);
+    // }
+	for(var i = 0; i < words.length - 1; i++){
+		var id = guid();
+		board.pushBlock(words[i].en, colors[board.getRandom(colors.length)-1], {id: id});
+    	board.pushBlock(words[i].kz, colors[board.getRandom(colors.length)-1], {id: id});
+	}
+	board.explode();
     // console.log(factory.canMoveRight(pushBlock4));
     //game.add.existing(mario);
 }
